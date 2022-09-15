bea546b679e9cb7828aef2b6cf6f45e03b459c6c
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-15 15:01:16 +0200

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,1 +1,9 @@
+#include "Receiver.h"
 
+void GetSensorOutput(int minValue, int maxValue, int * Array, int Range )
+{
+    for(int i =0; i < Range; i++)
+    {
+        Array[i] = (rand() % (maxValue - minValue+1) + minValue);
+    }
+}
