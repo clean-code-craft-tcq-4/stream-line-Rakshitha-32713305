a710f23223f996872e090cc93be6ffd28c1b5879
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-16 15:15:56 +0200

    Update testReceiver.cpp

--- testReceiver.cpp
+++ testReceiver.cpp
@@ -1,2 +1,18 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "catch/catch.hpp"
+#include "Receiver.h"
+
+TEST_CASE("positive case: test1 - reads the parameters from the console input") {
+  
+  BMSData_s sensorData_s;
+
+  sensorData_s.chargeRate_minvalue = 0;
+  sensorData_s.chargeRate_maxvalue = 100;
+  sensorData_s.temp_minvalue = 0;
+  sensorData_s.temp_maxvalue = 75;
+  sensorData_s.totalNoofValues = 50;
+  
+  Main(sensorData_s);  
+}
+
