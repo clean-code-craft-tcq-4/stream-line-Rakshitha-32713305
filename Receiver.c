5cf5d94f38999f570c553677f3e07877e6523c10
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-16 15:17:19 +0200

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,6 +1,6 @@
 #include "Receiver.h"
 
-void GetSensorOutput(int minValue, int maxValue, int * Array, int Range )
+void MGetSensorOutput(int minValue, int maxValue, int * Array, int Range )
 {
     for(int i =0; i < Range; i++)
     {
@@ -8,7 +8,7 @@ void GetSensorOutput(int minValue, int maxValue, int * Array, int Range )
     }
 }
 
-void PrintOnConsole(int * SensorData1 ,int * SensorData2 ,int Range)
+void MPrintOnConsole(int * SensorData1 ,int * SensorData2 ,int Range)
 {
   for(int data = 0; data < Range ; data ++)
   {
@@ -21,9 +21,9 @@ void Main(BMSData_s sensorData_s)
   int chargeRate[sensorData_s.totalNoofValues];
   int temperature[sensorData_s.totalNoofValues];
 
-  GetSensorOutput(sensorData_s.chargeRate_minvalue,sensorData_s.chargeRate_maxvalue,chargeRate,sensorData_s.totalNoofValues);
-  GetSensorOutput(sensorData_s.temp_minvalue,sensorData_s.temp_maxvalue,temperature,sensorData_s.totalNoofValues);
+  MGetSensorOutput(sensorData_s.chargeRate_minvalue,sensorData_s.chargeRate_maxvalue,chargeRate,sensorData_s.totalNoofValues);
+  MGetSensorOutput(sensorData_s.temp_minvalue,sensorData_s.temp_maxvalue,temperature,sensorData_s.totalNoofValues);
   
-  PrintOnConsole( &chargeRate[0], &temperature[0],sensorData_s.totalNoofValues);
+  MPrintOnConsole( &chargeRate[0], &temperature[0],sensorData_s.totalNoofValues);
  
 }
