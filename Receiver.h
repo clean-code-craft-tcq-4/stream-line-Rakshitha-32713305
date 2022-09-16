b6cd2e4e1ce94fa4f39be83cf2f707919374ec0e
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-16 15:16:56 +0200

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -9,8 +9,8 @@ int chargeRate_maxvalue ;
 int temp_minvalue; 
 int temp_maxvalue ;
 int totalNoofValues;
-}BMSData_s;
+}MBMSData_s;
 
-void GetSensorOutput(int minValue, int maxValue, int * Array, int Range );
-void PrintOnConsole(int * SensorData1 ,int * SensorData2 ,int Range);
+void MGetSensorOutput(int minValue, int maxValue, int * Array, int Range );
+void MPrintOnConsole(int * SensorData1 ,int * SensorData2 ,int Range);
 void Main(BMSData_s sensorData_s);
