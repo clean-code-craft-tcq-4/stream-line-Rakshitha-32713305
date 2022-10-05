c3080b4c231c5da12b9a219512fad01ce1e11f8c
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-05 15:36:19 +0200

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -3,14 +3,3 @@
 #include <string.h>
 
 
-typedef struct{
-int chargeRate_minvalue ;
-int chargeRate_maxvalue ;
-int temp_minvalue; 
-int temp_maxvalue ;
-int totalNoofValues;
-}MBMSData_s;
-
-void MGetSensorOutput(int minValue, int maxValue, int * Array, int Range );
-void MPrintOnConsole(int * SensorData1 ,int * SensorData2 ,int Range);
-void main();
