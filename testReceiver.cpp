ab5b6a108317d3bc4abc17cb577fe11080d68341
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-07 14:13:18 +0200

    Update testReceiver.cpp

--- testReceiver.cpp
+++ testReceiver.cpp
@@ -6,23 +6,7 @@
 
 TEST_CASE("Tests to check whether sensor data is read from console and also to check whether correct Max, Min and SMA values of sensor parameters are printed on console") 
 {
-  int *temp[50];
-  int *soc[50];
-  int i=0;
-  printf("Reading the data from console\n");
-  for(i=0; i<50; i++)
-  {
-    scanf("%d,%d\n", &temp[i], &soc[i]);
-  }
-    
-  
-  
-  printf("print the data from console\n");
-  for(i=0; i<50; i++)
-  {
-    printf("%d,%d\n", temp[i], soc[i]);
-  }
- 
+ int ret = main();
   
   
 }
