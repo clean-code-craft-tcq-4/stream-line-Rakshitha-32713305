5c5ce8d7c842e00b805319758de8f6a2aa83716a
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-07 21:28:15 +0200

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -7,5 +7,5 @@
 
 void ReadParameterFromConsole(int *temp, int *soc, int min, int max);
 void PrintDataOnConsole(int *temp, int *soc);
-void MinimumValueFromIncomingStream(int *data);
-void MaximumValueFromIncomingStream(int *data);
+int MinimumValueFromIncomingStream(int *data);
+int MaximumValueFromIncomingStream(int *data);
