f6c9908e60a999394d796bc9fde907482ed1751b
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-07 18:57:26 +0200

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -3,17 +3,17 @@
 void ReadParameterFromConsole(int *temp, int *soc)
 {
   int i;
-  int temp_buffer[count] =0;
-  int soc_buffer[count] =0;
+  //int temp_buffer[count] =0;
+  //int soc_buffer[count] =0;
   printf("Reading the data from console\n");
   for(i=0; i<count; i++)
   {
     scanf("%d,%d\n",&temp[i],&soc[i]);
-    temp_buffer[i] = temp[i];
-    soc_buffer[i] = soc[i];
+    //temp_buffer[i] = temp[i];
+    //soc_buffer[i] = soc[i];
   }
   
-  MinimumMaximumValueFromIncomingStream(temp_buffer, count);
+  MinimumMaximumValueFromIncomingStream(temp, count);
   MinimumMaximumValueFromIncomingStream(soc, count);
 }
 
@@ -30,8 +30,8 @@ void PrintDataOnConsole(int *temp, int *soc)
 void MinimumMaximumValueFromIncomingStream(int *data, int count)
 {
   int i;
-  int min[50]=data[0];
-  int max[50]=data[0];
+  int min=data[0];
+  int max=data[0];
   for(i=0; i<count; i++)
   {
     if(min>data[i])
