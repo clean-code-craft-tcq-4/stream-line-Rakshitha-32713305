cf0d8a708372f3c0b017fac9574a6a9231bca63b
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-07 21:18:05 +0200

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -4,7 +4,7 @@ void MinimumValueFromIncomingStream(int *data, int min)
 {
   int i;
   min=data[0];
-  for(i=0; i<count; i++)
+  for(i=0; i<50; i++)
   {
     if(data[i] < min)
     {
@@ -18,7 +18,7 @@ void MaximumValueFromIncomingStream(int *data, int max)
 {
   int i;
   max=data[0];
-  for(i=0; i<count; i++)
+  for(i=0; i<50; i++)
   {
     if(data[i] > max)
     {
@@ -38,12 +38,15 @@ void ReadParameterFromConsole(int *temp, int *soc, int min, int max)
   }
   
   MinimumValueFromIncomingStream(temp,min);
+  printf("minimum value of temperature is %d\n", min);
   MaximumValueFromIncomingStream(temp,max);
-  printf("minimum value of temperature is %d, and maximum value of temperature is %d\n", min, max);
+  printf("maximum value of temperature is %d\n", max);
+  
   
   MinimumValueFromIncomingStream(soc,min);
+  printf("minimum value of soc is %d\n", min);
   MaximumValueFromIncomingStream(soc,max);
-  printf("minimum value of soc is %d, and maximum value of soc is %d\n", min, max);
+  printf("maximum value of soc is %d\n", max);
 }
 
 void PrintDataOnConsole(int *temp, int *soc)
