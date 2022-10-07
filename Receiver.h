b79b5a23de32e20bb1cb6ebb91ad7d4bdede7138
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-07 11:21:10 +0200

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -5,9 +5,3 @@
 
 #define readings 50
 
-void readDataFromConsole(float* Temperature, float* SOC);
-float getMaxValue(float *sensorparameter);
-float getMinValue(float *sensorparameter);
-float calculateSimpleMovingAverage(float *sensorparameter);
-int printReceivedDataToConsole(float *sensorparameter, float maxvalue, float minvalue, float SMA);
-void receiveAndProcessSensorData(float* Temperature, float* SOC);
