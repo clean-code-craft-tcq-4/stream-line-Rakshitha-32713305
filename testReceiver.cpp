e1f24bfd04b09f4bb954126ad26be54a5d495fbd
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-10-07 21:00:27 +0200

    Update testReceiver.cpp

--- testReceiver.cpp
+++ testReceiver.cpp
@@ -8,8 +8,9 @@ TEST_CASE("Tests to check whether sensor data is read from console and also to c
 {
   int temp[count] = {0};
   int soc[count] = {0};
-  
- ReadParameterFromConsole(temp,soc);
+  int min = 0;
+  int max =0;
+ ReadParameterFromConsole(temp,soc,min,max);
  PrintDataOnConsole(temp,soc);
   
 }
